6b1e1db4e942a3d3887e5a792085ac2781dda810
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-12 14:45:53 +0530

    added import libraries to Gallery class

--- Backend/Backend/src/main/java/com/example/Backend/Model/Gallery.java
+++ Backend/Backend/src/main/java/com/example/Backend/Model/Gallery.java
@@ -1,4 +1,10 @@
 package com.example.Backend.Model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
+
+@Document(collection = "gallery") // Specify the collection name
 public class Gallery {
+
 }
