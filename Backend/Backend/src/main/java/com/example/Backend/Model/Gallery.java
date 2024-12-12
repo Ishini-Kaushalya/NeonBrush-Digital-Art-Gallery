bf9c05d1da798ea9184e4f0bf0685c20e8e206f5
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-12 14:47:35 +0530

    added attributes to the Gallery class

--- Backend/Backend/src/main/java/com/example/Backend/Model/Gallery.java
+++ Backend/Backend/src/main/java/com/example/Backend/Model/Gallery.java
@@ -1,10 +1,21 @@
 package com.example.Backend.Model;
-
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 
-@Document(collection = "gallery") // Specify the collection name
+@Document(collection = "gallery")// Specify the collection name
+@Data
 public class Gallery {
+    @Id
+    private long object_Id;
+    private long artist_Id;
+    private String title;
+    private String description;
+    private String artist;
+    private String type;
+    private double price;
+    private Date created_at;
+    private String is_available;
 
 }
