02244875e3438406f07042cc0ca84afca2b88dc1
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-12 14:51:07 +0530

    extended MongoRepository in GalleryRepository

--- Backend/Backend/src/main/java/com/example/Backend/Repository/GalleryRepository.java
+++ Backend/Backend/src/main/java/com/example/Backend/Repository/GalleryRepository.java
@@ -1,5 +1,6 @@
 package com.example.Backend.Repository;
 import com.example.Backend.Model.Gallery;
 import org.springframework.data.mongodb.repository.MongoRepository;
-public interface GalleryRepository {
+public interface GalleryRepository extends MongoRepository<Gallery, Long> {
+// You can define custom query methods here if needed
 }
