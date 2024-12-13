26f749c435bedcb978d515a983f898314fb55813
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-13 20:37:25 +0530

    added methods to the GalleryServiceImpl

--- Backend/Backend/src/main/java/com/example/Backend/Service/Impl/GalleryServiceImpl.java
+++ Backend/Backend/src/main/java/com/example/Backend/Service/Impl/GalleryServiceImpl.java
@@ -10,6 +10,32 @@ import java.util.Optional;
 
 @Service
 public class GalleryServiceImpl implements GalleryService{
+    @Autowired
+    private GalleryRepository galleryRepository;
+    @Override
+    public Gallery createGallery(Gallery gallery) {
+        return galleryRepository.save(gallery);
+    }
+    @Override
+    public Gallery getGalleryById(long id) {
+        Optional<Gallery> gallery = galleryRepository.findById(id);
+        return gallery.orElse(null);
+    }
+    @Override
+    public List<Gallery> getAllGallery() {
+        return galleryRepository.findAll();
+    }
+    @Override
+    public Gallery updateGallery(long id, Gallery gallery) {
+        if (galleryRepository.existsById(id)) { gallery.setObject_Id(id);
+            return galleryRepository.save(gallery);
+        }
+        return null; // Return null or throw an exception if not found
+    }
+    @Override
+    public void deleteGallery(long id) {
+        galleryRepository.deleteById(id);
+    }
 
 
 }
