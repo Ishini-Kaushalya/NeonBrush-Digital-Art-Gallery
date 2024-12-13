b95b85462fd9706e23439f9beb0f43c7e68c99e0
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-13 20:39:27 +0530

    added the calling methods to the GalleryService class

--- Backend/Backend/src/main/java/com/example/Backend/Service/GalleryService.java
+++ Backend/Backend/src/main/java/com/example/Backend/Service/GalleryService.java
@@ -3,4 +3,9 @@ package com.example.Backend.Service;
 import com.example.Backend.Model.Gallery;
 import java.util.List;
 public interface GalleryService {
+    Gallery createGallery(Gallery gallery);
+    Gallery getGalleryById(long id);
+    List<Gallery> getAllGallery();
+    Gallery updateGallery(long id, Gallery gallery);
+    void deleteGallery(long id);
 }
