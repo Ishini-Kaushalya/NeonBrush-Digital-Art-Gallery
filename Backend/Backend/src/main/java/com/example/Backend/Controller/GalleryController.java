19ff0d39f8cec76c2678d7b97349ab296af92289
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-13 20:44:36 +0530

    created the API methods in GalleryController

--- Backend/Backend/src/main/java/com/example/Backend/Controller/GalleryController.java
+++ Backend/Backend/src/main/java/com/example/Backend/Controller/GalleryController.java
@@ -13,5 +13,37 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/galleries")
+
 public class GalleryController {
+    @Autowired
+    private GalleryService galleryService;
+    public GalleryController() {
+    }
+    @PostMapping
+    public ResponseEntity<Gallery> createGallery(@RequestBody Gallery gallery) {
+        Gallery createdGallery = this.galleryService.createGallery(gallery);
+        return ResponseEntity.ok(createdGallery);
+    }
+    @GetMapping({"/{id}"})
+    public ResponseEntity<Gallery> getGalleryById(@PathVariable long id) {
+        Gallery gallery = this.galleryService.getGalleryById(id);
+        return gallery != null ? ResponseEntity.ok(gallery) :ResponseEntity.notFound().build();
+    }
+    @GetMapping
+    public ResponseEntity<List<Gallery>> getAllGallery() {
+        List<Gallery> gallery = this.galleryService.getAllGallery();
+        return ResponseEntity.ok(gallery);
+    }
+    @PutMapping({"/{id}"})
+    public ResponseEntity<Gallery> updateGallery(@PathVariable long id, @RequestBody Gallery gallery) {
+        Gallery updatedGallery = this.galleryService.updateGallery(id, gallery);
+        return updatedGallery != null ? ResponseEntity.ok(updatedGallery) :ResponseEntity.notFound().build();
+    }
+    @DeleteMapping({"/{id}"})
+    public ResponseEntity<Void> deleteGallery(@PathVariable long id) {
+        this.galleryService.deleteGallery(id);
+        return ResponseEntity.noContent().build();
+    }
 }
