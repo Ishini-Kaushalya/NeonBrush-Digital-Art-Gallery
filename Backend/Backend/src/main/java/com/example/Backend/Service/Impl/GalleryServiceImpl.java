b5d834ed01b8fee6307217ce07648d6ec93dbb6c
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-13 20:36:13 +0530

    added some libraries and implemented another class for GalleryServiceImpl

--- Backend/Backend/src/main/java/com/example/Backend/Service/Impl/GalleryServiceImpl.java
+++ Backend/Backend/src/main/java/com/example/Backend/Service/Impl/GalleryServiceImpl.java
@@ -5,5 +5,11 @@ import com.example.Backend.Repository.GalleryRepository;
 import com.example.Backend.Service.GalleryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-public class GalleryServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GalleryServiceImpl implements GalleryService{
+
+
 }
