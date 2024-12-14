42de7b802b90cf48fc0830993382a1d85773e6be
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-14 21:16:07 +0530

    imported libraries to the Payment class

--- Backend/Backend/src/main/java/com/example/Backend/Model/Payment.java
+++ Backend/Backend/src/main/java/com/example/Backend/Model/Payment.java
@@ -1,4 +1,8 @@
 package com.example.Backend.Model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 public class Payment {
 }
