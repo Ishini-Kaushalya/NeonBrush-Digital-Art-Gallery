974714a38569b4f600a4c7018a2199d58e851c23
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-14 21:24:59 +0530

    added method implementations to the PaymentServiceImpl

--- Backend/Backend/src/main/java/com/example/Backend/Service/Impl/PaymentServiceImpl.java
+++ Backend/Backend/src/main/java/com/example/Backend/Service/Impl/PaymentServiceImpl.java
@@ -8,5 +8,32 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-public class PaymentServiceImpl {
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    private PaymentRepository paymentRepository;
+    @Override
+    public Payment createPayment(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+    @Override
+    public Payment getPaymentById(long id) {
+        Optional<Payment> payment = paymentRepository.findById(id);
+        return payment.orElse(null);
+    }
+    @Override
+    public List<Payment> getAllPayment() {
+        return paymentRepository.findAll();
+    }
+    @Override
+    public Payment updatePayment(long id, Payment payment) {
+        if (paymentRepository.existsById(id)) { payment.setPayment_id(id);
+            return paymentRepository.save(payment);
+        }
+        return null; // Return null or throw an exception if not found
+    }
+    @Override
+    public void deletePayment(long id) {
+        paymentRepository.deleteById(id);
+    }
 }
