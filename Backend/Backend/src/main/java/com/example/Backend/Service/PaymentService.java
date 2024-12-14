a11bb94a5f564a9885aec6f820577caa41dc2cf6
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-14 21:22:05 +0530

    added methods to the PaymentService

--- Backend/Backend/src/main/java/com/example/Backend/Service/PaymentService.java
+++ Backend/Backend/src/main/java/com/example/Backend/Service/PaymentService.java
@@ -1,4 +1,13 @@
 package com.example.Backend.Service;
 
+import com.example.Backend.Model.Payment;
+import java.util.List;
+
 public interface PaymentService {
+
+    Payment createPayment(Payment payment);
+    Payment getPaymentById(long id);
+    List<Payment> getAllPayment();
+    Payment updatePayment(long id, Payment payment);
+    void deletePayment(long id);
 }
