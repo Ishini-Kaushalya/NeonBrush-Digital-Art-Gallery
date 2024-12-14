18a01f26fc5c650677af2c29a257431e0f152194
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-14 21:19:33 +0530

    added API methods to the PaymentController class

--- Backend/Backend/src/main/java/com/example/Backend/Controller/PaymentController.java
+++ Backend/Backend/src/main/java/com/example/Backend/Controller/PaymentController.java
@@ -12,5 +12,38 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/payment")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+    public PaymentController() {
+    }
+    @PostMapping
+    public ResponseEntity<Payment> createPayment(@RequestBody Payment payment) {
+        Payment createdPayment = this.paymentService.createPayment(payment);
+        return ResponseEntity.ok(createdPayment);
+    }
+    @GetMapping({"/{id}"})
+    public ResponseEntity<Payment> getPaymentById(@PathVariable long id) {
+        Payment payment = this.paymentService.getPaymentById(id);
+        return payment != null ? ResponseEntity.ok(payment) :ResponseEntity.notFound().build();
+    }
+    @GetMapping
+    public ResponseEntity<List<Payment>> getAllPayment() {
+        List<Payment> payment = this.paymentService.getAllPayment();
+        return ResponseEntity.ok(payment);
+    }
+    @PutMapping({"/{id}"})
+    public ResponseEntity<Payment> updatePayment(@PathVariable long id, @RequestBody Payment payment) {
+        Payment updatedPayment = this.paymentService.updatePayment(id, payment);
+        return updatedPayment != null ? ResponseEntity.ok(updatedPayment) :ResponseEntity.notFound().build();
+    }
+    @DeleteMapping({"/{id}"})
+    public ResponseEntity<Void> deletePayment(@PathVariable long id) {
+        this.paymentService.deletePayment(id);
+        return ResponseEntity.noContent().build();
+    }
 }
