e6787037d050e81e297b0bf68d0860734cf965b5
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-17 11:39:54 +0530

    changed the pointilism image

--- Frontend/src/Components/ExploreMenu.jsx
+++ Frontend/src/Components/ExploreMenu.jsx
@@ -3,10 +3,13 @@ import { menu_list } from "../assets/Common/assets";
 
 const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div className='flex flex-col gap-5 mt-5 mb-[-90px]' id='explore-menu'>
-      <h1 className='text-black font-medium text-lg'>Explore our menu</h1>
+    <div
+      className="flex flex-col gap-5 items-center mt-5 mb-[-90px]"
+      id="explore-menu"
+    >
+      <h1 className="text-black font-medium text-lg">Explore our menu</h1>
 
-      <p className='max-w-[60%] text-gray-500 md:max-w-full md:text-sm'>
+      <p className="max-w-[60%] text-gray-500 md:max-w-full md:text-sm">
         Welcome to our digital art gallery, where creativity knows no bounds.
         Explore our curated exhibitions featuring a diverse array of
         contemporary artists, each showcasing their unique vision and style.
@@ -17,7 +20,7 @@ const ExploreMenu = ({ category, setCategory }) => {
         art in the digital age!
       </p>
 
-      <div className='flex justify-between items-center gap-7 text-center my-5 overflow-x-auto  scrollbar-hide'>
+      <div className="flex justify-between items-center gap-7 text-center my-5 overflow-x-auto  scrollbar-hide">
         {menu_list.map((item, index) => {
           return (
             <div
@@ -27,7 +30,7 @@ const ExploreMenu = ({ category, setCategory }) => {
                 )
               }
               key={index}
-              className='flex flex-col items-center cursor-pointer'
+              className="flex flex-col items-center cursor-pointer"
             >
               <img
                 className={`w-[7.5vw] min-w-[80px] rounded-full transition-all duration-200 ${
@@ -38,7 +41,7 @@ const ExploreMenu = ({ category, setCategory }) => {
                 src={item.menu_image}
                 alt={item.menu_name}
               />
-              <p className='mt-2 text-gray-500 text-[max(1.4vw,16px)]'>
+              <p className="mt-2 text-gray-500 text-[max(1.4vw,16px)]">
                 {item.menu_name}
               </p>
             </div>
