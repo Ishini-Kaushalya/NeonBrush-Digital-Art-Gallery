aa095d35f52000d5918d74ba7d204b76e633a082
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 13:00:05 +0530

    created a functional component to display artItems

--- Frontend/src/Components/ArtDisplay.jsx
+++ Frontend/src/Components/ArtDisplay.jsx
@@ -1,3 +1,6 @@
 import React, { useContext } from "react";
 import { StoreContext } from "../Context/StoreContext";
 import ArtItem from "./ArtItem";
+
+const ArtDisplay = ({ category }) => {
+  const { art_list } = useContext(StoreContext);
