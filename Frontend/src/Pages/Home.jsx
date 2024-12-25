ed1dc455a0ee28b10e010df7960acee517431a34
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 13:16:44 +0530

    added products file navitagation to home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
-import Footer from "../Components/Footer";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+
 import Header from "../Components/Header";
 import ExploreMenu from "../Components/ExploreMenu";
 import ArtDisplay from "../Components/ArtDisplay";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
+  const navigate = useNavigate(); // For navigation
+
+  const handleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+    navigate("/products", { state: { category: newCategory } }); // Navigate to Products page with category
+  };
+
   return (
-    <>
+    <div>
       <Header />
-      <ExploreMenu category={category} setCategory={setCategory} />
-    </>
+      <ExploreMenu category={category} setCategory={handleCategoryChange} />
+      {/* <ArtDisplay category={category} /> */}
+    </div>
   );
 };
 
