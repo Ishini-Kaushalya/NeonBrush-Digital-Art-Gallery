c823b13e95db5d0de87173489cbb5286eaad346b
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 12:13:24 +0530

    added imported libraries to the product page

--- Frontend/src/Pages/Products.jsx
+++ Frontend/src/Pages/Products.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import ArtItem from "../Components/ArtItem"; 
+import { art_list } from "../assets/Common/assets";
 
 const Products = () => {
   return <div>Products</div>;
