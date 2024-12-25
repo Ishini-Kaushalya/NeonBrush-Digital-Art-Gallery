3fd06fa07237629f5a1aa6a10b12bddb8aa7464d
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 13:19:57 +0530

    added router for main file

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import './index.css'
+import "./index.css";
 import StoreContextProvider from "./Context/StoreContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
 );
