03b8742c1fb54ac7e76edf3b8ce2e704b0dc22d5
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 13:26:07 +0530

    call products in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,33 @@
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Products from "./Pages/Products";
 import Cart from "./Pages/Cart";
-import SignInSelection from "./Pages/SignInSelection";
-import { useState } from "react";
-import LoginPopup from "./Components/LoginPopup";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import PlaceOrder from "./Pages/PlaceOrder";
+import SignInSelection from "./Pages/SignInSelection"; // Import the new page
+import LoginPopup from "./Components/LoginPopup";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
       {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : null}
-      <div className='app'>
+      <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='/cart' element={<Cart />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/products" element={<Products />} />{" "}
+          {/* Product Route */}
           <Route
-            path='/sign-in'
+            path="/sign-in"
             element={<SignInSelection setShowLogin={setShowLogin} />}
-          />
+          />{" "}
+          {/* New route */}
         </Routes>
       </div>
       <Footer />
