f683d814d1bdd09538aeeb5affde3348824a6369
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 12:39:47 +0530

    added addToCart functionality

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -4,4 +4,17 @@ import { art_list } from "../assets/Common/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState({});
+    const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
