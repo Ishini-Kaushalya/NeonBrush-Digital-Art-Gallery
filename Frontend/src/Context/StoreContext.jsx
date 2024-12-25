8e06edf04a9b72d762ea5d7c06f2c64c1559b156
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 12:38:36 +0530

    Created StoreContext and StoreContextProvider component to manage cart state

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -1,2 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { art_list } from "../assets/Common/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
