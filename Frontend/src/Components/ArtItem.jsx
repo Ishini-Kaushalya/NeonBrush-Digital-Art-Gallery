84fe9660e68685335c1572661aa3c5b5c4185aaf
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 12:31:52 +0530

    added functions in ArtItem file

--- Frontend/src/Components/ArtItem.jsx
+++ Frontend/src/Components/ArtItem.jsx
@@ -1,3 +1,6 @@
 import React, { useContext } from "react";
 import { assets } from "../assets/Common/assets";
 import { StoreContext } from "../Context/StoreContext";
+
+const ArtItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
