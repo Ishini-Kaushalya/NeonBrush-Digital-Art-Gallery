da811882e461bc8bb03538940e9638324dc8b717
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 13:13:50 +0530

    created a promo code section and cart total

--- Frontend/src/Pages/Cart.jsx
+++ Frontend/src/Pages/Cart.jsx
@@ -43,3 +43,48 @@ const Cart = () => {
           return null;
         })}
       </div>
+      <div className="mt-20 flex flex-col lg:flex-row gap-12">
+        <div className="flex-1 space-y-4">
+          <h2 className="text-lg font-semibold">Cart Total</h2>
+          <div className="space-y-2 text-gray-600">
+            <div className="flex justify-between">
+              <p>Subtotal</p>
+              <p>$0</p>
+            </div>
+            <hr className="border-gray-300" />
+            <div className="flex justify-between">
+              <p>Delivery Fee</p>
+              <p>$2</p>
+            </div>
+            <hr className="border-gray-300" />
+            <div className="flex justify-between font-bold">
+              <p>Total</p>
+              <p>$0</p>
+            </div>
+          </div>
+          <button className="w-full max-w-xs bg-tomato text-white py-3 rounded">
+            PROCEED TO CHECKOUT
+          </button>
+        </div>
+
+        <div className="flex-1 space-y-4">
+          <p className="text-gray-600">
+            If you have a promo code, enter it here
+          </p>
+          <div className="flex items-center bg-gray-200 rounded">
+            <input
+              type="text"
+              placeholder="Promo code"
+              className="flex-grow bg-transparent px-3 py-2 focus:outline-none"
+            />
+            <button className="bg-black text-white px-6 py-2 rounded-r">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
