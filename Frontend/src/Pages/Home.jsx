2fbe77661ebdb6187ec5dde016663848acc83824
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2024-12-25 13:15:52 +0530

    imported ArtDisplay to home file

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import ExploreMenu from "../Components/ExploreMenu";
+import ArtDisplay from "../Components/ArtDisplay";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
