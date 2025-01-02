21b89c00916f806e1eb14adb32872878770f73fb
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-01-02 13:13:03 +0530

    changed explore menu description

--- Frontend/src/Components/ExploreMenu.jsx
+++ Frontend/src/Components/ExploreMenu.jsx
@@ -4,17 +4,24 @@ import { menu_list } from "../assets/Common/assets";
 const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div
-      className='flex flex-col text-center gap-6 mt-5 mb-[-70px] px-5'
-      id='explore-menu'
+      className="flex flex-col text-center gap-6 mt-5 mb-[-70px] px-5"
+      id="explore-menu"
     >
-      <h1 className='text-black font-bold text-[max(2vw,22px)]'>
-        Explore our menu
+      <h1 className="text-black font-bold text-[max(2vw,22px)]">
+        The Art Discovery Zone
       </h1>
-      <p className='max-w-[90%] text-center text-gray-600 leading-[1.8] text-[max(1vw,16px)] md:max-w-full'>
-        Welcome to our digital art gallery...
+      <p className="max-w-[90%] text-center text-gray-600 leading-[1.8] text-[max(1vw,16px)] md:max-w-full">
+        Unleash your creativity and immerse yourself in our vibrant collection
+        of digital arts. From stunning illustrations and captivating animations
+        to innovative graphic designs and interactive installations, our curated
+        selection showcases the limitless possibilities of digital expression.
+        Whether you're an artist seeking inspiration or an enthusiast eager to
+        discover new talent, our platform invites you to explore, engage, and
+        experience the dynamic intersection of technology and creativity. Join
+        us on this artistic journey and let your imagination soar!
       </p>
 
-      <div className='flex justify-center items-center gap-5 text-center my-5 overflow-x-auto scrollbar-hide'>
+      <div className="flex justify-center items-center gap-5 text-center my-5 overflow-x-auto scrollbar-hide">
         {menu_list.map((item, index) => (
           <div
             onClick={() => setCategory(item.menu_name)} // Set the category here
