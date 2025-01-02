272b5ed406414662c5031e581ee29fd31477c334
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-01-02 21:37:56 +0530

    added findByArtistId method to ReviewRepository

--- Backend/Backend/src/main/java/com/example/Backend/Repository/ReviewRepository.java
+++ Backend/Backend/src/main/java/com/example/Backend/Repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.example.Backend.Repository;
 
 import com.example.Backend.Model.Review;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
 public interface ReviewRepository extends MongoRepository<Review, Long> {
+    List<Review> findByArtistId(Long artist_Id);
 }
