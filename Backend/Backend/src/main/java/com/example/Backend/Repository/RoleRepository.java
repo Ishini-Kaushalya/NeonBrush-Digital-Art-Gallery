8668ce662217606f22a21fecf23f95467cfbe68c
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-01-08 16:35:42 +0530

    added libraries to RoleRepository

--- Backend/Backend/src/main/java/com/example/Backend/Repository/RoleRepository.java
+++ Backend/Backend/src/main/java/com/example/Backend/Repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.example.Backend.Repository;
 
-public interface RoleRepository {
+        import java.util.Optional;
+        import org.springframework.data.mongodb.repository.MongoRepository;
+        import com.example.Backend.Model.ERole;
+        import com.example.Backend.Model.Role;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(ERole name);
 }
