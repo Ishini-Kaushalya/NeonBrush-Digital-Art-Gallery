52e405ae92bf7d7d04991cc7a7c12b9ecffbe94f
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-01-08 17:13:50 +0530

    changed comments in WebSecurityConfig

--- Backend/Backend/src/main/java/com/example/Backend/Security/WebSecurityConfig.java
+++ Backend/Backend/src/main/java/com/example/Backend/Security/WebSecurityConfig.java
@@ -5,12 +5,11 @@ package com.example.Backend.Security;
         import org.springframework.context.annotation.Configuration;
         import org.springframework.security.authentication.AuthenticationManager;
         import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
         import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
         import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
         import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
         import org.springframework.security.config.http.SessionCreationPolicy;
         import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
         import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,12 +24,10 @@ package com.example.Backend.Security;
 
 
 @Configuration
-//@EnableWebSecurity
+
 @EnableMethodSecurity
-//(securedEnabled = true,
-//jsr250Enabled = true,
-//prePostEnabled = true) // by default
-public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
+
+public class WebSecurityConfig  {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
 
@@ -42,10 +39,6 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         return new AuthTokenFilter();
     }
 
-//@Override
-//public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//  authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//}
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
@@ -57,11 +50,6 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         return authProvider;
     }
 
-//@Bean
-//@Override
-//public AuthenticationManager authenticationManagerBean() throws Exception {
-//  return super.authenticationManagerBean();
-//}
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
@@ -73,17 +61,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-//@Override
-//protected void configure(HttpSecurity http) throws Exception {
-//  http.cors().and().csrf().disable()
-//    .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-//    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-//    .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-//    .antMatchers("/api/test/**").permitAll()
-//    .anyRequest().authenticated();
-//
-//  http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//}
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
