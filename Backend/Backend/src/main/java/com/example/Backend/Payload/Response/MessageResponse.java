ad455ae8b40723cca513b0424afaa5d6c6160a1e
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-01-08 16:34:18 +0530

    added attributes to MessageResponse class

--- Backend/Backend/src/main/java/com/example/Backend/Payload/Response/MessageResponse.java
+++ Backend/Backend/src/main/java/com/example/Backend/Payload/Response/MessageResponse.java
@@ -1,4 +1,17 @@
 package com.example.Backend.Payload.Response;
 
 public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
