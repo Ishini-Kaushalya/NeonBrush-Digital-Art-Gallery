d0481b7235f5da0830ae7a4b8da00fa2e35ae688
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-01-18 13:29:46 +0530

    change attributes in contact class

--- Backend/Backend/src/main/java/com/example/Backend/Model/Contact.java
+++ Backend/Backend/src/main/java/com/example/Backend/Model/Contact.java
@@ -7,48 +7,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Contact {
     @Id
     private long contactId;
-    private long fullName;
-    private int email;
-    private double phoneNumber;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
     private String message;
 
-    public long getContactId() {
-        return contactId;
-    }
 
-    public void setContactId(long contactId) {
-        this.contactId = contactId;
-    }
-
-    public long getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(long fullName) {
-        this.fullName = fullName;
-    }
-
-    public int getEmail() {
-        return email;
-    }
-
-    public void setEmail(int email) {
-        this.email = email;
-    }
-
-    public double getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(double phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
