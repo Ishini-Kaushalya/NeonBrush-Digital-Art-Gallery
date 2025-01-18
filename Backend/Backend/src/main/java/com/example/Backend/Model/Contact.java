875dc1c2fe57eb9ebea52e5dcae25fee7976d155
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-01-18 13:06:45 +0530

    added attributes to contact class

--- Backend/Backend/src/main/java/com/example/Backend/Model/Contact.java
+++ Backend/Backend/src/main/java/com/example/Backend/Model/Contact.java
@@ -1,4 +1,54 @@
 package com.example.Backend.Model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Contact")
 public class Contact {
+    @Id
+    private long contactId;
+    private long fullName;
+    private int email;
+    private double phoneNumber;
+    private String message;
+
+    public long getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(long contactId) {
+        this.contactId = contactId;
+    }
+
+    public long getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(long fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getEmail() {
+        return email;
+    }
+
+    public void setEmail(int email) {
+        this.email = email;
+    }
+
+    public double getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(double phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
