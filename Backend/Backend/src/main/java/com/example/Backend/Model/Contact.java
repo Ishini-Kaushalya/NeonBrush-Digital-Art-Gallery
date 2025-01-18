3673381f7e88461ca958d3e3ed74c9ee0c0b7514
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-01-18 13:30:43 +0530

    added getters and setters into contact class

--- Backend/Backend/src/main/java/com/example/Backend/Model/Contact.java
+++ Backend/Backend/src/main/java/com/example/Backend/Model/Contact.java
@@ -12,5 +12,43 @@ public class Contact {
     private String phoneNumber;
     private String message;
 
+    public long getContactId() {
+        return contactId;
+    }
 
+    public void setContactId(long contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
