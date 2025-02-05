30470cec2233c722b096223b97029bec4efd8ff7
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-02-05 11:56:18 +0530

    changed payment related classes

--- Backend/Backend/src/main/java/com/example/Backend/Repository/PaymentRepository.java
+++ Backend/Backend/src/main/java/com/example/Backend/Repository/PaymentRepository.java
@@ -2,5 +2,5 @@ package com.example.Backend.Repository;
 import com.example.Backend.Model.Payment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PaymentRepository extends MongoRepository<Payment,Long> {
+public interface PaymentRepository extends MongoRepository<Payment,String> {
 }
