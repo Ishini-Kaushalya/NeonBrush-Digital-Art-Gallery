0712039495d90f0700f58b596e93c3bfc55ecb22
Nelu <neluni09@gmail.com>
2025-03-11 15:06:55 +0530

    test

--- Frontend/src/Pages/Admin.jsx
+++ Frontend/src/Pages/Admin.jsx
@@ -188,7 +188,6 @@ const AdminPage = () => {
           ))}
         </ul>
       </div>
-
       {/* Added Art Section at the Bottom */}
       <div className="mt-8">
         <h2 className="text-xl font-semibold mb-2">Recently Added Art Items</h2>
