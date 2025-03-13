35471987f0cb0058f9ffd159daa1e0899daa1e4e
Ishini-Kaushalya <kaushal-ps20106@stu.kln.ac.lk>
2025-03-13 22:00:22 +0530

    changed navbar

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -12,16 +12,18 @@ import { jwtDecode } from "jwt-decode";
 const Navbar = () => {
   const [loggedInUsername, setLoggedInUsername] = useState();
   const [menu, setMenu] = useState("home");
-  const { clearCart, getCartSize } = useContext(StoreContext);
-  const [isArtist, setIsArtist] = useState(false); // State to check if the user is an artist
-  const [loading, setLoading] = useState(true); // State to track loading status
-  const [isSignedIn, setIsSignedIn] = useState(false); // State to track authentication status
+  const { getCartSize } = useContext(StoreContext);
+  const [isArtist, setIsArtist] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [notification, setNotification] = useState("");
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [hasProfile, setHasProfile] = useState(false); // State to track if the user has a profile
   const location = useLocation();
   const navigate = useNavigate();
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef(null);
 
-  // Close dropdown when clicking outside
   useEffect(() => {
     function handleClickOutside(event) {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -33,28 +35,27 @@ const Navbar = () => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  // Check if the user is signed in on component mount
   useEffect(() => {
     const token = JSON.parse(localStorage.getItem("accessToken"));
-    setIsSignedIn(!!token); // Set isSignedIn to true if token exists
+    setIsSignedIn(!!token);
   }, []);
 
-  // Fetch the current user's role
   useEffect(() => {
-    const fetchUserRole = async () => {
+    const fetchUserRoleAndProfile = async () => {
       try {
         const token = JSON.parse(localStorage.getItem("accessToken"));
 
         if (!token) {
           console.error("No token found, please login first.");
-          setIsArtist(false); // Assume the user is not an artist if no token is found
+          setIsArtist(false);
           return;
         }
+
         const decodedToken = jwtDecode(token);
-        const username = decodedToken.sub || decodedToken.username; // Adjust based on your token structure
+        const username = decodedToken.sub || decodedToken.username;
         setLoggedInUsername(username);
 
-        // Fetch the user's role
+        // Fetch user role
         const roleResponse = await axios.get(
           "http://localhost:8080/api/user/role",
           {
@@ -63,21 +64,40 @@ const Navbar = () => {
             },
           }
         );
-
-        // Check if the user is an artist
         setIsArtist(roleResponse.data.includes("ROLE_ARTIST"));
+
+        // Check if the user has a profile
+        if (roleResponse.data.includes("ROLE_ARTIST")) {
+          try {
+            const profileResponse = await axios.get(
+              `http://localhost:8080/api/artist/username/${username}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+            setHasProfile(!!profileResponse.data); // Set hasProfile to true if profile exists
+          } catch (error) {
+            if (error.response?.status === 404) {
+              setHasProfile(false); // No profile found
+            } else {
+              console.error("Error fetching profile:", error);
+            }
+          }
+        }
       } catch (error) {
-        console.error("Error fetching user role:", error);
-        setIsArtist(false); // Assume the user is not an artist if there's an error
+        console.error("Error fetching user role or profile:", error);
+        setIsArtist(false);
+        setHasProfile(false);
       } finally {
-        setLoading(false); // Set loading to false after the request completes
+        setLoading(false);
       }
     };
 
-    fetchUserRole();
+    fetchUserRoleAndProfile();
   }, []);
 
-  // Update menu state based on the current location (URL)
   useEffect(() => {
     if (location.pathname === "/") {
       setMenu("home");
@@ -104,34 +124,64 @@ const Navbar = () => {
     }
   }, [location.pathname, loggedInUsername]);
 
-  // Handle sign out
   const handleSignOut = () => {
-    localStorage.removeItem("accessToken"); // Remove the token from localStorage
-    setIsSignedIn(false); // Update authentication status
-    setIsArtist(false); // Reset artist status
-    clearCart();
-    navigate("/"); // Redirect to home page
+    localStorage.removeItem("accessToken");
+    setIsSignedIn(false);
+    setIsArtist(false);
+    setHasProfile(false);
+    navigate("/");
   };
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
+
   const closeDropdown = () => {
     setDropdownOpen(false);
   };
 
+  const handleDeleteProfile = async () => {
+    setConfirmDelete(true);
+  };
+
+  const confirmDeleteProfile = async () => {
+    try {
+      const token = JSON.parse(localStorage.getItem("accessToken"));
+      const response = await axios.delete(
+        `http://localhost:8080/api/artist/username/${loggedInUsername}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setNotification("Profile and all artworks deleted successfully!");
+      setHasProfile(false); // Update hasProfile state
+      handleSignOut(); // Sign out the user after deleting the profile
+    } catch (error) {
+      console.error("Error deleting profile:", error);
+      setNotification("Failed to delete profile. Please try again.");
+    } finally {
+      setConfirmDelete(false);
+    }
+  };
+
+  const cancelDeleteProfile = () => {
+    setConfirmDelete(false);
+  };
+
   if (loading) {
-    return <div>Loading...</div>; // Show a loading spinner or message
+    return <div>Loading...</div>;
   }
 
   return (
-    <div className='flex justify-between items-center px-24 py-5 mb-[-40px]'>
-      <Link to='/'>
-        <img src={assets.logo} alt='Logo' className='w-[80px]' />
+    <div className="flex justify-between items-center px-24 py-5 mb-[-40px]">
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="w-[80px]" />
       </Link>
-      <ul className='flex gap-12 text-[#49557e] text-lg'>
+      <ul className="flex gap-12 text-[#49557e] text-lg">
         <Link
-          to='/'
+          to="/"
           onClick={() => setMenu("home")}
           className={`cursor-pointer ${
             menu === "home" ? "border-b-2 pb-1 border-sky-600" : ""
@@ -140,7 +190,7 @@ const Navbar = () => {
           Home
         </Link>
         <Link
-          to='/products'
+          to="/products"
           onClick={() => setMenu("menu")}
           className={`cursor-pointer ${
             menu === "arts" ? "border-b-2 pb-1 border-sky-600" : ""
@@ -150,7 +200,7 @@ const Navbar = () => {
         </Link>
         {loggedInUsername === "admin" && (
           <Link
-            to='/order-detail'
+            to="/order-detail"
             onClick={() => setMenu("order-details")}
             className={`cursor-pointer ${
               menu === "order-details" ? "border-b-2 pb-1 border-sky-600" : ""
@@ -161,7 +211,7 @@ const Navbar = () => {
         )}
         {loggedInUsername !== "admin" && (
           <Link
-            to='/contact-us'
+            to="/contact-us"
             onClick={() => setMenu("contact-us")}
             className={`cursor-pointer ${
               menu === "contact-us" ? "border-b-2 pb-1 border-sky-600" : ""
@@ -170,11 +220,9 @@ const Navbar = () => {
             Contact us
           </Link>
         )}
-
-        {/* Conditionally render Notification button for artists */}
         {isArtist && (
           <Link
-            to='/notifications'
+            to="/notifications"
             onClick={() => setMenu("notifications")}
             className={`cursor-pointer ${
               menu === "notifications" ? "border-b-2 pb-1 border-sky-600" : ""
@@ -183,9 +231,8 @@ const Navbar = () => {
             Notification
           </Link>
         )}
-        {/* Add Artists Link */}
         <Link
-          to='/show-artist'
+          to="/show-artist"
           onClick={() => setMenu("artists")}
           className={`cursor-pointer ${
             menu === "artists" ? "border-b-2 pb-1 border-[#38bdf8]" : ""
@@ -195,10 +242,10 @@ const Navbar = () => {
         </Link>
       </ul>
 
-      <div className='flex items-center gap-10'>
-        <div className='relative'>
-          <Link to='/cart' className='cursor-pointer'>
-            <TbBasketFilled className='w-[30px] h-[30px] text-gray-700' />
+      <div className="flex items-center gap-10">
+        <div className="relative">
+          <Link to="/cart" className="cursor-pointer">
+            <TbBasketFilled className="w-[30px] h-[30px] text-gray-700" />
           </Link>
           <div
             className={
@@ -208,20 +255,18 @@ const Navbar = () => {
             }
           ></div>
         </div>
-        {/* Conditionally render Sign In or Sign Out button */}
         {isSignedIn ? (
           <>
             <button
               onClick={handleSignOut}
-              className='bg-transparent text-[#49557e] text-base border-2 border-tomato rounded-full py-2 px-7 cursor-pointer transition duration-300 ease-in-out hover:bg-[#f0f9ff] hover:border-[#49557e]'
+              className="bg-transparent text-[#49557e] text-base border-2 border-tomato rounded-full py-2 px-7 cursor-pointer transition duration-300 ease-in-out hover:bg-[#f0f9ff] hover:border-[#49557e]"
             >
               Sign Out
             </button>
-            {/* Conditionally render the Artist Profile button */}
             {isArtist && (
-              <div className='relative'>
+              <div className="relative">
                 <FaUserCircle
-                  className='w-[30px] h-[30px] text-gray-700 cursor-pointer'
+                  className="w-[30px] h-[30px] text-gray-700 cursor-pointer"
                   onClick={toggleDropdown}
                 />
                 {dropdownOpen && (
@@ -231,41 +276,65 @@ const Navbar = () => {
                     animate={{ x: 0, opacity: 1 }}
                     exit={{ x: 100, opacity: 0 }}
                     transition={{ duration: 0.3, ease: "easeOut" }}
-                    className='absolute right-0 top-0 mt-2 bg-white shadow-lg rounded-lg border border-gray-300 z-10 w-48'
+                    className="absolute right-0 top-0 mt-2 bg-white shadow-lg rounded-lg border border-gray-300 z-10 w-48"
                   >
-                    <ul className='flex flex-col'>
+                    <ul className="flex flex-col">
                       <li>
                         <Link
-                          to='/artist-profile'
+                          to={`/artist-detail/${loggedInUsername}`}
                           onClick={closeDropdown}
-                          className='block px-4 py-2 text-gray-700 hover:bg-gray-100'
+                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
                         >
                           My Profile
                         </Link>
                       </li>
                       <li>
                         <Link
-                          to='/artist-artworks'
+                          to="/artist-profile"
                           onClick={closeDropdown}
-                          className='block px-4 py-2 text-gray-700 hover:bg-gray-100'
+                          className={`block px-4 py-2 text-gray-700 ${
+                            hasProfile
+                              ? "opacity-50 cursor-not-allowed"
+                              : "hover:bg-gray-100"
+                          }`}
+                          style={{
+                            pointerEvents: hasProfile ? "none" : "auto",
+                          }} // Disable click if hasProfile is true
+                        >
+                          Create Profile
+                        </Link>
+                      </li>
+                      <li>
+                        <button
+                          onClick={handleDeleteProfile}
+                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100 w-full text-left"
+                        >
+                          Delete Profile
+                        </button>
+                      </li>
+                      <li>
+                        <Link
+                          to="/artist-artworks"
+                          onClick={closeDropdown}
+                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
                         >
                           My Artworks
                         </Link>
                       </li>
                       <li>
                         <Link
-                          to='/add-art'
+                          to="/add-art"
                           onClick={closeDropdown}
-                          className='block px-4 py-2 text-gray-700 hover:bg-gray-100'
+                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
                         >
                           Add Art
                         </Link>
                       </li>
                       <li>
                         <Link
-                          to='/order-detail'
+                          to="/order-detail"
                           onClick={closeDropdown}
-                          className='block px-4 py-2 text-gray-700 hover:bg-gray-100'
+                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
                         >
                           See Orders
                         </Link>
@@ -277,13 +346,42 @@ const Navbar = () => {
             )}
           </>
         ) : (
-          <Link to='/sign-in'>
-            <button className='bg-transparent text-[#49557e] text-base border-2 border-tomato rounded-full py-2 px-7 cursor-pointer transition duration-300 ease-in-out hover:bg-[#f0f9ff] hover:border-[#49557e]'>
+          <Link to="/sign-in">
+            <button className="bg-transparent text-[#49557e] text-base border-2 border-tomato rounded-full py-2 px-7 cursor-pointer transition duration-300 ease-in-out hover:bg-[#f0f9ff] hover:border-[#49557e]">
               Sign In
             </button>
           </Link>
         )}
       </div>
+      {notification && (
+        <div className="fixed top-4 right-4 bg-white border border-gray-300 shadow-lg rounded-lg p-4">
+          <p>{notification}</p>
+        </div>
+      )}
+      {confirmDelete && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <p className="mb-4">
+              Are you sure you want to delete your profile and all associated
+              artworks?
+            </p>
+            <div className="flex justify-end">
+              <button
+                onClick={confirmDeleteProfile}
+                className="px-4 py-2 mr-2 text-white bg-red-500 rounded hover:bg-red-600"
+              >
+                Yes
+              </button>
+              <button
+                onClick={cancelDeleteProfile}
+                className="px-4 py-2 text-gray-700 bg-gray-300 rounded hover:bg-gray-400"
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- Frontend/src/Pages/ArtistDetail.jsx
+++ Frontend/src/Pages/ArtistDetail.jsx
@@ -88,7 +88,7 @@ const ArtistDetail = () => {
         if (error.response?.status === 401) {
           setError("Unauthorized: Please log in again.");
         } else {
-          setError("An error occurred while fetching artist details.");
+          setError("Not yet create an account ? Create now.");
         }
       } finally {
         setLoading(false); // Set loading to false after the request completes
@@ -124,21 +124,21 @@ const ArtistDetail = () => {
 
   if (loading) {
     return (
-      <div className='flex justify-center items-center h-screen'>
-        <div className='w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin'></div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
       </div>
     );
   }
 
   if (error) {
     return (
-      <div className='text-center mt-16'>
-        <p className='text-red-500'>{error}</p>
+      <div className="text-center mt-16">
+        <p className="text-red-500">{error}</p>
         <button
-          className='bg-black text-white py-2 px-6 rounded-lg shadow-md hover:bg-blue-600 mt-4'
-          onClick={() => navigate("/login")} // Redirect to login page
+          className="bg-black text-white py-2 px-6 rounded-lg shadow-md hover:bg-blue-600 mt-4"
+          onClick={() => navigate("/artist-profile")} // Redirect to login page
         >
-          Log In
+          Create Account
         </button>
       </div>
     );
@@ -163,10 +163,10 @@ const ArtistDetail = () => {
   };
 
   return (
-    <div className='artist-detail container mx-auto mt-16 p-4'>
+    <div className="artist-detail container mx-auto mt-16 p-4">
       {/* Back Button */}
       <button
-        className='bg-black text-white py-2 px-6 rounded-lg shadow-md hover:bg-sky-950 mb-4'
+        className="bg-black text-white py-2 px-6 rounded-lg shadow-md hover:bg-sky-950 mb-4"
         onClick={() => navigate(-1)}
       >
         Back
@@ -174,39 +174,39 @@ const ArtistDetail = () => {
 
       {/* Content Area */}
       <div
-        className='relative flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8 bg-cover bg-center p-8 rounded-lg'
+        className="relative flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8 bg-cover bg-center p-8 rounded-lg"
         style={{
           backgroundImage: `url(${imageUrl})`, // Use the fetched image as the background
         }}
       >
         {/* Transparent Overlay for background */}
-        <div className='absolute inset-0 bg-black opacity-70 backdrop-blur-10 rounded-lg'></div>
+        <div className="absolute inset-0 bg-black opacity-70 backdrop-blur-10 rounded-lg"></div>
 
         {/* Left Side: Artist Image */}
-        <div className='relative z-10'>
+        <div className="relative z-10">
           <img
             src={imageUrl} // Use the fetched image URL
             alt={artist.userName}
-            className='w-[300px] h-[300px] object-cover rounded-lg shadow-lg border-8 border-white'
+            className="w-[300px] h-[300px] object-cover rounded-lg shadow-lg border-8 border-white"
           />
-          <h2 className='text-3xl font-semibold mt-4 text-center text-white'>
+          <h2 className="text-3xl font-semibold mt-4 text-center text-white">
             {artist.userName}
           </h2>
         </div>
 
         {/* Right Side: Artist Details */}
-        <div className='relative z-10 space-y-4 bg-opacity-90 p-6 rounded-lg shadow-lg'>
-          <h1 className='text-4xl font-bold text-white'>About the Artist</h1>
-          <p className='text-lg text-white'>{artist.description}</p>
+        <div className="relative z-10 space-y-4 bg-opacity-90 p-6 rounded-lg shadow-lg">
+          <h1 className="text-4xl font-bold text-white">About the Artist</h1>
+          <p className="text-lg text-white">{artist.description}</p>
         </div>
       </div>
       {/* Artist's Artworks */}
       {artworks.length > 0 ? (
-        <div className='mt-8'>
-          <h2 className='text-2xl font-bold mb-4'>Artworks</h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+        <div className="mt-8">
+          <h2 className="text-2xl font-bold mb-4">Artworks</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
             {artworks.map((art) => (
-              <div key={art._id} className='relative'>
+              <div key={art._id} className="relative">
                 <ArtItem
                   id={art._id}
                   name={art.title}
@@ -218,7 +218,7 @@ const ArtistDetail = () => {
                 {/* Delete Icon */}
                 {isArtist && loggedInUsername === id && (
                   <button
-                    className='absolute bottom-4 right-4 bg-red-500 text-white p-2 rounded-full hover:bg-red-600'
+                    className="absolute bottom-4 right-4 bg-red-500 text-white p-2 rounded-full hover:bg-red-600"
                     onClick={() => deleteArtwork(art.title)} // Call delete function
                   >
                     <FaTrash />
@@ -229,15 +229,8 @@ const ArtistDetail = () => {
           </div>
         </div>
       ) : (
-        <p className='text-gray-600'>No artworks available for this artist.</p>
+        <p className="text-gray-600">No artworks available for this artist.</p>
       )}
-      {/* Back Button */}
-      <button
-        className='text-black py-4 px-8 text-xl rounded-lg shadow-md hover:bg-gray-800 hover:text-white mb-4'
-        onClick={() => navigate(-1)}
-      >
-        <IoChevronBackCircleOutline className='mr-2 text-3xl' />
-      </button>
     </div>
   );
 };
